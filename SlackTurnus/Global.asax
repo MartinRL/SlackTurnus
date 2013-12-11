d00ce91df33e4011d530af59ef8f18aebfe3fc0a
Martin Rosén-Lidholm <mrol@telenor.dk>
2013-12-11 12:56:23 +0100

    VS13 generated MVC boiler plate

---
+++ SlackTurnus/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SlackTurnus.MvcApplication" Language="C#" %>
---
+++ SlackTurnus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlackTurnus.Startup))]
+namespace SlackTurnus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
