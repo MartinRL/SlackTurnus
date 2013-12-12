0b9bf7b40d6121c919b3691e902d8de7096063fa
Martin Rosén-Lidholm <mrol@telenor.dk>
2013-12-12 11:14:43 +0100

    impl. Next

--- SlackTurnus/Controllers/HomeController.cs
+++ SlackTurnus/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
-using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using SlackTurnus.DomainModel;
 
 namespace SlackTurnus.Controllers
@@ -10,10 +12,12 @@ namespace SlackTurnus.Controllers
 	public class HomeController : Controller
 	{
 		private readonly IGetSlackTurnus _getSlackTurnus;
+		private readonly IUpdateSlackTurnus _updateSlackTurnus;
 
-		public HomeController(IGetSlackTurnus getSlackTurnus)
+		public HomeController(IGetSlackTurnus getSlackTurnus, IUpdateSlackTurnus updateSlackTurnus)
 		{
 			_getSlackTurnus = getSlackTurnus;
+			_updateSlackTurnus = updateSlackTurnus;
 		}
 
 		public ActionResult Index()
@@ -23,7 +27,36 @@ namespace SlackTurnus.Controllers
 
 		public ActionResult Next()
 		{
-			throw new NotImplementedException();
+			var slackTurnus = _getSlackTurnus.Execute();
+
+			var lastIndex = slackTurnus.Count - 1;
+
+			var firstInLineSlacker = slackTurnus.Cast<DictionaryEntry>().Last();
+
+			slackTurnus.RemoveAt(lastIndex);
+
+			slackTurnus.Insert(0, firstInLineSlacker.Key, firstInLineSlacker.Value);
+
+			_updateSlackTurnus.Execute(slackTurnus);
+
+			return RedirectToAction("Index");
+		}
+	}
+
+	public interface IUpdateSlackTurnus
+	{
+		void Execute(IOrderedDictionary slackTurnus);
+	}
+
+	public class UpdateSlackTurnus : IUpdateSlackTurnus
+	{
+		public void Execute(IOrderedDictionary slackTurnus)
+		{
+			// 2do: DI
+			using (var streamWriter = new StreamWriter(HostingEnvironment.MapPath("~/DomainModel/slackTurnus.json")))
+			{
+				streamWriter.Write(JsonConvert.SerializeObject(slackTurnus));
+			}
 		}
 	}
 }
